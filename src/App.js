f63060d5e0b300c323e6d5f18c33aefb1a89c8a0
Heung-e-ya <hoonun1@naver.com>
2023-11-30 10:02:15 +0900

    file check

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import "./App.css";
 import MainPage from "./main";
 import { Route, Routes } from "react-router-dom";
 import ProductPage from "./product";
-
 import UploadPage from "./upload";
 function App() {
   return (
     <div>
-      <Routes>
-        <Route exat={true} element={<MainPage />} path="/" />
-        <Route exat={true} element={<ProductPage />} path="/product/:id" />
-        <Route exat={true} element={<UploadPage />} path="/upload" />
-      </Routes>
+      <div id="header">
+        <div id="header-area">
+          <img src="images/icons/logo.png" />
+        </div>
+      </div>
+      <div id="body">
+        <Routes>
+          <Route exat={true} element={<MainPage />} path="/" />
+          <Route exat={true} element={<ProductPage />} path="/product/:id" />
+          <Route exat={true} element={<UploadPage />} path="/upload" />
+        </Routes>
+      </div>
+      <div id="footer"></div>
     </div>
   );
 }
--- src/product/index.js
+++ src/product/index.js
@@ -1,11 +1,29 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 const ProductPage = () => {
   const { id } = useParams();
+  const [product, setProduct] = useState(null);
   console.log(id);
+  useEffect(function () {
+    axios
+      .get(
+        `https://90c0e3b5-9a21-4c91-b303-f52ffff433ae.mock.pstmn.io/products/${id}`
+      )
+      .then(function (result) {
+        console.log(result);
+        setProduct(result.data);
+      })
+      .catch(function (error) {
+        console.error("에러 발생 : ", error);
+      });
+  }, []);
+  console.log(product);
   return (
     <div>
-      <h1>상품페이지 {id} 입니다.</h1>
+      <div id="image-box">
+        <img src={product.imageUrl} />
+      </div>
     </div>
   );
 };
